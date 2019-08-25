01098bfde31d88ebf8d1ef29a836025b67b08f0d
RafitJ <rafit.jamil@gmail.com>
2019-08-25 03:29:06 -0400

    final lambda update + ui

--- aws/realm-classify-img/lambda_function.py
+++ aws/realm-classify-img/lambda_function.py
@@ -1,40 +1,40 @@
-import os
-import io
-import boto3
 import json
-import requests
-import pandas as pd
-import requests
+import boto3
+import base64
+
 
 sagemaker_url = "https://runtime.sagemaker.us-east-1.amazonaws.com/endpoints/realm-image-classifier/invocations"
-# grab environment variables
-runtime = boto3.client('runtime.sagemaker')
+
+
 
 def lambda_handler(event, context):
-    print("Received event: " + json.dumps(event, indent=2))
+    image = base64.b64decode(event)
     
-    data = json.loads(json.dumps(event))
-    url = data['data']
+    runtime = boto3.Session().client(service_name='sagemaker-runtime', region_name='us-east-1')
+    response = runtime.invoke_endpoint(EndpointName=sagemaker_url, ContentType='application/x-image', Body=image)
+    probs = response['Body'].read().decode() 
 
-    f = requests.get(url)
-    # separate each line
-    newf = f.text.splitlines()
-    # create pandas dataframe
-    df = pd.DataFrame([x.split(",") for x in newf])
+    probs = ast.literal_eval(probs) 
+    probs = np.array(probs)
 
-    img_file = {'media': df}
-    # requests.post(sagemaker_url, files=img_file)
+    topk_indexes = probs.argsort() 
+    topk_indexes = topk_indexes[::-1][:topk] 
 
-    response = runtime.invoke_endpoint(EndpointName="realm-image-classifier",
-                                       ContentType='text/csv',
-                                       Body=img_file)
-    print(response)
-    result = json.loads(response['Body'].read().decode())
-    print(result)
-    pred = int(result['predictions'][0]['predicted_label'])
-    predicted_label = 'M' if pred == 1 else 'B'
+    topk_categories = []
+    for i in topk_indexes:
+       topk_categories.append((i+1, probs[i]))
     
+    # response = runtime.invoke_endpoint(EndpointName="realm-image-classifier",
+    #                                   ContentType='image/png',
+    #                                   Body=event)
+                                       
+    # result = json.loads(response['Body'].read().decode())
+    
+    # pred = int(result['predictions'])
+
+    
+    # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps(predicted_label)
+        'body': str(topk_categories)
     }
