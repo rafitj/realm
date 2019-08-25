dd58a7c4c01f75cde670ec12deb6604b4101ed83
RafitJ <rafit.jamil@gmail.com>
2019-08-24 21:08:51 -0400

    move lambda stuff

--- aws/realm-classify-img/lambda_function.py
+++ aws/realm-classify-img/lambda_function.py
@@ -2,36 +2,32 @@ import os
 import io
 import boto3
 import json
-from PIL import Image
-from numpy import array, moveaxis, indices, dstack
-from pandas import DataFrame
+import requests
+import pandas as pd
+import requests
 
-
-def convert_to_csv(data):
-    image = Image.open(data)
-    pixels = image.convert("RGB")
-    rgbArray = array(pixels.getdata()).reshape(image.size + (3,))
-    indicesArray = moveaxis(indices(image.size), 0, 2)
-    allArray = dstack((indicesArray, rgbArray)).reshape((-1, 5))
-
-
-    df = DataFrame(allArray, columns=["y", "x", "red","green","blue"])
-    print(df.head())
-    df.to_csv("data.csv",index=False)
-    return df
-
+sagemaker_url = "https://runtime.sagemaker.us-east-1.amazonaws.com/endpoints/realm-image-classifier/invocations"
 # grab environment variables
-runtime= boto3.client('runtime.sagemaker')
+runtime = boto3.client('runtime.sagemaker')
 
 def lambda_handler(event, context):
     print("Received event: " + json.dumps(event, indent=2))
     
     data = json.loads(json.dumps(event))
-    payload = data['data']
-    img = convert_to_csv(payload)
+    url = data['data']
+
+    f = requests.get(url)
+    # separate each line
+    newf = f.text.splitlines()
+    # create pandas dataframe
+    df = pd.DataFrame([x.split(",") for x in newf])
+
+    img_file = {'media': df}
+    # requests.post(sagemaker_url, files=img_file)
+
     response = runtime.invoke_endpoint(EndpointName="realm-image-classifier",
                                        ContentType='text/csv',
-                                       Body=img)
+                                       Body=img_file)
     print(response)
     result = json.loads(response['Body'].read().decode())
     print(result)
